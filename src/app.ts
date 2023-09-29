9dbe381e0a92dd337738a5d1268f1f261e7169c9
Anastasiya Ramanava1 <Anastasiya_Ramanava1@epam.com>
2023-09-29 12:48:00 +0300

    feat: add hobbies APIs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import http from 'http';
 import { getUsers, postUsers, getUser, patchUser, deleteUser } from './users/routeHandler';
+import { deleteHobbyForUser, getHobbiesForUser, postHobbyForUser } from './hobby/routeHandler';
 
 const port = 8000;
 const host = 'localhost';
@@ -17,6 +18,13 @@ const server = http.createServer((req, res) => {
     if (req.method === 'PATCH') patchUser(req, res);
     // DELETE /users/:id
     if (req.method === 'DELETE') deleteUser(req, res);
+  } else if (req.url?.match(/^\/hobbies\?user=(\d+)/)) {
+    // GET /hobbies?user=:id
+    if (req.method === 'GET') getHobbiesForUser(req, res);
+    // POST /hobbies?user=:id
+    if (req.method === 'POST') postHobbyForUser(req, res);
+    // DELETE /hobbies?user=:id
+    if (req.method === 'DELETE') deleteHobbyForUser(req, res);
   } else {
     res.writeHead(400);
     res.end(JSON.stringify({ message: 'Route not found' }));
---
+++ src/hobby/routeHandler.ts
@@ -0,0 +1,72 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import { getRequestData } from '../utils';
+import * as service from './service';
+
+function getUserIdFromUrl(url: string): number {
+  try {
+    const queryParams = url.split('?')[1];
+    const userQueryParam = new URLSearchParams(queryParams).get('user');
+
+    const isUserQueryParamValid = /^\d+$/.test(userQueryParam || '');
+
+    if (!isUserQueryParamValid) throw new Error("Can't parse user id");
+    return Number(userQueryParam);
+  } catch {
+    throw new Error("Error during user id parsing");
+  }
+}
+
+export function getHobbiesForUser(req: IncomingMessage, res: ServerResponse) {
+  try {
+    const userId = getUserIdFromUrl(req.url || '');
+
+    try {
+      const hobbies = service.getHobbies(userId);
+      res.writeHead(200);
+      res.end(JSON.stringify(hobbies));
+    } catch (e) {
+      res.writeHead(404);
+      res.end(JSON.stringify({ message: (e as Error).message }));
+    }
+  } catch (e) {
+    res.writeHead(500);
+    res.end(JSON.stringify({ message: (e as Error).message }));
+  }
+}
+
+export async function postHobbyForUser(req: IncomingMessage, res: ServerResponse) {
+  try {
+    const userId = getUserIdFromUrl(req.url || '');
+
+    const data = await getRequestData(req);
+    const newHobby = JSON.parse(data);
+
+    const hobby = service.addHobby(userId, newHobby);
+    res.writeHead(201);
+    res.end(JSON.stringify({ message: `Hobby ${newHobby} was added for user ${userId}` }));
+  } catch (e) {
+    res.writeHead(500);
+    res.end(JSON.stringify({ message: (e as Error).message }));
+  }
+}
+
+export async function deleteHobbyForUser(req: IncomingMessage, res: ServerResponse) {
+  try {
+    const userId = getUserIdFromUrl(req.url || '');
+
+    const data = await getRequestData(req);
+    const hobby = JSON.parse(data);
+
+    try {
+      service.deleteHobby(userId, hobby);
+      res.writeHead(200);
+      res.end(JSON.stringify({ message: `Hobby ${hobby} was deleted for user ${userId}` }));
+    } catch (e) {
+      res.writeHead(404);
+      res.end(JSON.stringify({ message: (e as Error).message }));
+    }
+  } catch (e) {
+    res.writeHead(500);
+    res.end(JSON.stringify({ message: (e as Error).message }));
+  }
+}
